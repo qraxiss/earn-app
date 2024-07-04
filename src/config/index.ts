f48fa27ebcb18cd7f9f81ea04809392d753dbba8
qraxiss <qraxiss@gmail.com>
2024-07-04 15:04:38 +0300

    feat: wrong api

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
-export default {
-  earnUrl: "http://localhost:1338/api",
-  authUrl: "http://localhost:1337/api",
-};
-
 // export default {
-//   earnUrl: "https://api.shopcek.com/api",
-//   authUrl: "https://api.shopcek.com/auth/api",
+//   earnUrl: "http://localhost:1338/api",
+//   authUrl: "http://localhost:1337/api",
 // };
+
+export default {
+  earnUrl: "https://api.shopcek.com/api",
+  authUrl: "https://api.shopcek.com/auth/api",
+};
