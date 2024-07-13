666c44684e47db61e865fdf918fe81b655cf40cb
qraxiss <qraxiss@gmail.com>
2024-07-13 14:11:00 +0300

    feat: locked, if balance not enough

--- src/components/earn/mine/product.tsx
+++ src/components/earn/mine/product.tsx
@@ -8,6 +8,8 @@ import { AppDispatch } from "../../../store";
 import { buy as buyApi, upgrade as upgradeApi } from "../../../slices/card/api";
 import formatNumber from "../../../helpers/format-number";
 import { useRef } from "react";
+import { xpSelector } from "../../../slices/xp/slice";
+import { useSelector } from "react-redux";
 
 export default function Product({
   product: { info, status, upgrade, buy },
@@ -22,10 +24,13 @@ export default function Product({
 const Buy = ({ info, status, buy }: any) => {
   const dispatch: AppDispatch = useDispatch();
   const ref = useRef(null);
+  const { point } = useSelector(xpSelector);
+
+  const canBuy = buy.price > point;
+
   const onClick = () => {
     const div = ref.current; // corresponding DOM node
     if (div) {
-      console.log(div);
       let className = (div as any).className as string;
 
       if (className.includes("product-hover")) {
@@ -33,7 +38,6 @@ const Buy = ({ info, status, buy }: any) => {
       } else {
         className += className + " product-hover";
       }
-      console.log(className);
       (div as any).className = className;
     }
   };
@@ -77,20 +81,38 @@ const Buy = ({ info, status, buy }: any) => {
         <div
           className={`buy-button d-flex flex-column justify-content-center align-items-center`}
           onClick={async (e) => {
-            e.stopPropagation();
-            await dispatch(buyApi.initiate({ cardId: info.id }));
+            if (canBuy) {
+              e.stopPropagation();
+              return;
+            }
+
+            const div = ref.current; // corresponding DOM node
+            if (div) {
+              let className = (div as any).className as string;
+              if (className.includes("product-hover")) {
+                await dispatch(buyApi.initiate({ cardId: info.id }));
+              }
+            }
           }}
         >
           <div className="d-flex align-items-center gap-2">
             <h5 className="buy-heading">BUY</h5>
-            {status.locked ? <Image src={Lock} className="buy-lock" /> : ""}
+            {status.locked || canBuy ? (
+              <Image src={Lock} className="buy-lock" />
+            ) : (
+              ""
+            )}
           </div>
           <div
             className={`m-1 d-flex justify-content-center align-items-center ${
               status.locked ? "price-container" : ""
             }`}
           >
-            <img className="dollar me-2" src={logo} alt="" />
+            <img
+              className="dollar me-2"
+              src={!canBuy ? logo : EarningLogo}
+              alt=""
+            />
             <h4 className="m-0">{formatNumber(buy.price)}</h4>
           </div>
         </div>
@@ -109,10 +131,11 @@ const Buy = ({ info, status, buy }: any) => {
 const Upgrade = ({ info, status, upgrade }: any) => {
   const dispatch: AppDispatch = useDispatch();
   const ref = useRef(null);
+  const { point } = useSelector(xpSelector);
+
   const onClick = () => {
     const div = ref.current; // corresponding DOM node
     if (div) {
-      console.log(div);
       let className = (div as any).className as string;
 
       if (className.includes("product-hover")) {
@@ -120,10 +143,11 @@ const Upgrade = ({ info, status, upgrade }: any) => {
       } else {
         className += className + " product-hover";
       }
-      console.log(className);
       (div as any).className = className;
     }
   };
+
+  const canBuy = upgrade.price > point;
 
   return (
     <div
@@ -152,26 +176,48 @@ const Upgrade = ({ info, status, upgrade }: any) => {
         <div className="image-container">
           <h6 className="hourly-income">Hourly Rental Income</h6>
           <h6 className="d-flex justify-content-center align-items-center">
-            <img className="earning-logo me-1" src={EarningLogo} alt="" />+{" "}
-            {formatNumber(upgrade.profit)} /h
+            <img className={"earning-logo me-1"} src={EarningLogo} alt="" />
+            <div className={!canBuy ? "" : " not-enough"}>
+              +{formatNumber(upgrade.profit)} /h
+            </div>
           </h6>
         </div>
 
         <div
           className={`buy-button d-flex flex-column justify-content-center align-items-center`}
           onClick={async (e) => {
-            e.stopPropagation();
-            await dispatch(upgradeApi.initiate({ cardId: info.id }));
+            if (canBuy) {
+              e.stopPropagation();
+              return;
+            }
+
+            const div = ref.current; // corresponding DOM node
+            if (div) {
+              let className = (div as any).className as string;
+              if (className.includes("product-hover")) {
+                await dispatch(upgradeApi.initiate({ cardId: info.id }));
+              }
+            }
           }}
         >
           <div className="d-flex align-items-center gap-2">
             <h5 className="buy-heading">UPGRADE</h5>
-            {status.locked ? <Image src={Lock} className="buy-lock" /> : ""}
+            {status.locked || canBuy ? (
+              <Image src={Lock} className="buy-lock" />
+            ) : (
+              ""
+            )}
           </div>
           <div
-            className={`m-1 d-flex justify-content-center align-items-center`}
+            className={`m-1 d-flex justify-content-center align-items-center ${
+              !canBuy ? "" : "not-enough"
+            }`}
           >
-            <img className="dollar me-2" src={logo} alt="" />
+            <img
+              className={`dollar me-2 ${!canBuy ? "" : " not-enough"}`}
+              src={!canBuy ? logo : EarningLogo}
+              alt=""
+            />
             <h4 className="m-0">{formatNumber(upgrade.price)}</h4>
           </div>
         </div>
